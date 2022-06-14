e682b00e092db07840808a7c7156098d604d6238
Leonardo Freitas <geeksleo@gmail.com>
2022-06-14 14:54:30 -0300

    Implementação de modelo e classe de persistencia contendo interface, metodo e construtor.

---
+++ Models/Cliente.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api_crud.Models
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+
+        [Required, StringLength(50)]
+        public string Nome { get; set; }
+
+        public string Sobrenome { get; set; }
+
+    }
+}
---
+++ Repository/ClienteRepository.cs
@@ -0,0 +1,56 @@
+﻿using api_crud.Models;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api_crud.Repository
+{
+
+    public interface IClienteRepository
+    {
+        Cliente Selecionar(int id);
+        IEnumerable<Cliente> Listar();
+        void Persistir(Cliente cliente);
+        void Atualizar(Cliente cliente);
+        void Excluir(int id);
+    }
+
+
+    public class ClienteRepository : IClienteRepository
+    {
+        private IConfiguration _configuracoes;
+        private string _conexao { get { return _configuracoes.GetConnectionString("mysqldb"); } }
+
+        public ClienteRepository(IConfiguration config)
+        {
+            _configuracoes = config;
+        }
+
+        public Cliente Selecionar(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Cliente> Listar()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Persistir(Cliente cliente)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Atualizar(Cliente cliente)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Excluir(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
